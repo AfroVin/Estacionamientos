6134d54a0844a21048d26dc6647ee08e8d27fd30
shyy guyy <wladimir.acevedo.237@gmail.com>
2022-04-30 17:34:50 -0400

    login

--- js/validarLogin.js
+++ js/validarLogin.js
@@ -1,19 +1,21 @@
-
-
 function Login() {
     var correo = document.login.email.value;
     var pass = document.login.contra.value;
     
-    if (correo=="correoprueba@correo.com" && pass=="123"){
+    if (correo=="correoprueba@correo.com" && pass=="1234567890"){
       
-        //arreglar url
-         window.location.assign("https://1.ooskar.com")
-  }  
+        //navegar a url
+        const path = location.pathname;
+        const pathNav = path.slice(1,-10)+'index.html';
+         window.location.assign(pathNav);
+        
+  }  if (correo !="correoprueba@correo.com"){
+    window.alert('Correo Invalido')
+  }if (pass!="1234567890"){
+    window.alert('Contraseña Invalida')
+  }
 
-
-
-
-    }
+}
     
 
 
