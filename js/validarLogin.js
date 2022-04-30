f5cf5bcdd2c714ce1fc3ec5b86dd0854e1246065
shyy guyy <wladimir.acevedo.237@gmail.com>
2022-04-30 00:09:30 -0400

    add login

---
+++ js/validarLogin.js
@@ -0,0 +1,22 @@
+
+
+function Login() {
+    var correo = document.login.email.value;
+    var pass = document.login.contra.value;
+    
+    if (correo=="correoprueba@correo.com" && pass=="123"){
+      
+        //arreglar url
+         window.location.assign("https://1.ooskar.com")
+  }  
+
+
+
+
+    }
+    
+
+
+
+
+
